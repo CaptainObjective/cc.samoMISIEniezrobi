77075ee35ca638651dc3a95af17626b12a2287cc
Jan Poniatowski <poniatowski126@gmail.com>
2018-12-17 10:54:24 +0100

    Kolorki i ikony
    -Dodałem w stylach zmienne z kolorami
    -Zamieniłem obrazki checklisty na ikony

--- src/js/taskGroup.js
+++ src/js/taskGroup.js
@@ -23,7 +23,7 @@ class taskGroup {
     render() {
         // Oczywiscie caly ten HTML można zmienić ja to zakodowalem tylko by pokazać koncepcje
 
-        this.element.innerHTML = `<img src = './src/img/checklist-${this.bgcolor}.png' alt = 'checklist-icon'/>
+        this.element.innerHTML = `<i class="fas fa-clipboard-list"></i>
                                     <h2>${this.taskGroupName}</h2>
                                     <div class="line"></div>
                                     <div class = "progressBarBorder"></div>
@@ -32,39 +32,5 @@ class taskGroup {
     }
 
 }
-
-// class taskGroup {
-
-//     constructor(taskGroupID, taskGroupName, taskGroupDes, taskGroupStyle, bgcolor) {
-//         console.log('taskGroup Loaded...');
-//         this.taskGroupID = taskGroupID;
-//         this.taskGroupName = taskGroupName;
-//         this.taskGroupDes = taskGroupDes; // zamiast tego jakiś obrazek moze iść
-//         this.taskGroupStyle = taskGroupStyle; // customowe style do napisana w SCSS tu tylko przekazać nazwe klasy, ewentualnie mozna to wywalić i ostylować ID
-//         this.bgcolor = bgcolor; //kolor tła z bootstrapa
-//         this.element = document.createElement("div");
-//         this.element.id = this.taskGroupID;
-//         this.element.className = `card text-white ${this.bgcolor} mb-3 ${this.taskGroupStyle}`
-//         this.element.onclick = this.click;
-//     }
-//     click = () => {
-//         const taskView = new TaskView(this.taskGroupName, this.bgcolor);
-//         taskView.render();
-//     }
-//     render() {
-//         // Oczywiscie caly ten HTML można zmienić ja to zakodowalem tylko by pokazać koncepcje
-
-//         this.element.innerHTML = `<div class="card-header">Karta zadań</div>
-//                                     <div class="card-body">
-//                                     <h5 class="card-title">${this.taskGroupName}</h5>
-//                                     <p class="card-text">${this.taskGroupDes}</p>`
-//         document.getElementById('root').appendChild(this.element);
-//     }
-
-// }
-
-
-
-
 
 export default taskGroup;
