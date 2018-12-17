88c27db8411cac90aae9fdf4d1d31f976757f82e
Jan Poniatowski <poniatowski126@gmail.com>
2018-12-17 11:54:07 +0100

    Plusik

--- src/js/mainView.js
+++ src/js/mainView.js
@@ -7,9 +7,10 @@ class MainView {
         this.tasklist3 = ['zadanie1', 'zadanie2', 'zadanie3'];
         console.log('MainView Loaded...');
         this.work = new TaskGroup(`work`, 'Praca', `Some quick example text to build on the card title and make up the bulk of the card's
-        content.`, 'taskGroup', 'pink', this.tasklist1.length);
-        this.shop = new TaskGroup(`shop`, 'Lista zakupów', ``, 'taskGroup', 'orange', this.tasklist2.length);
-        this.visit = new TaskGroup(`visits`, 'Do Odwiedzenia', ``, 'taskGroup', 'green', this.tasklist3.length);
+        content.`, 'list', 'pink', this.tasklist1.length);
+        this.shop = new TaskGroup(`shop`, 'Lista zakupów', ``, 'list', 'orange', this.tasklist2.length);
+        this.visit = new TaskGroup(`visits`, 'Do Odwiedzenia', ``, 'list', 'green', this.tasklist3.length);
+        this.add = new TaskGroup('', 'Dodaj', '', 'add', 'blue', 0);
     }
 
     render() {
@@ -17,6 +18,7 @@ class MainView {
         this.work.render();
         this.shop.render();
         this.visit.render();
+        this.add.render();
         //To Do: kolejna/e karta z zadaniami wg schematu
         //Może by kiedyś dodać zawsze na ostatnim miejscu karte z plusikiem która pozwoli robić nowe grupy tasków (zobaczymy na co backend pozwoli)
     }
