14bda6548f19732dfbd508299a18f0ab7f716308
anna <anna.lokietko@gmail.com>
2018-12-17 18:13:59 +0100

    icons

--- src/js/task.js
+++ src/js/task.js
@@ -14,11 +14,28 @@ class Task {
         //co ma sie zadziać po kliknieciu
         console.log(this);
     }
+
+zdarzenie(e){
+    e.stopPropagation();
+e.target.parentNode.firstChild.style.textDecoration = "line-throughe.";
+}
+
+     
+
     render() {
-        this.element.innerHTML = `<p>${this.name}</p>`;
-        this.element.innerHTML += `<button class="button">asdasdsds</button>`;
+        this.element.innerHTML = `<button class="button done"><img class="icons" src="./src/img/png/003-check.png"/></button>
+        <p>${this.name}</p> 
+        <button class="button"> <img  class="icons"  src="./src/img/png/001-rubbish.png"/></button> <button class="button">
+        <img class="icons" src="./src/img/png/002-doodles.png"/></button>`;
         document.getElementById('root').appendChild(this.element);
-    }
+      };
+    
+    
+
+    
+       
+
+
 }
 
 export default Task;
