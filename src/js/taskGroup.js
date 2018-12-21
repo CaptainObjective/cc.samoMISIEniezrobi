694c5051c3f0c499810e4f04b0ac4a6ceacaf2a4
Jan Poniatowski <poniatowski126@gmail.com>
2018-12-21 12:23:36 +0100

    Pasek postępu

--- src/js/taskGroup.js
+++ src/js/taskGroup.js
@@ -2,12 +2,11 @@ import TaskView from './taskView';
 
 class taskGroup {
 
-    constructor(taskGroupID, taskGroupName, taskGroupStyle, bgcolor, taskLength, taskList) {
+    constructor(taskGroupID, taskGroupName, taskGroupStyle, bgcolor, taskList) {
         console.log('taskGroup Loaded...');
         this.taskGroupID = taskGroupID;
         this.taskGroupName = taskGroupName;
         this.taskGroupStyle = taskGroupStyle;// customowe style do napisana w SCSS tu tylko przekazać nazwe klasy, ewentualnie mozna to wywalić i ostylować ID
-        this.taskLength = taskLength;
         this.taskList = taskList;
         this.bgcolor = bgcolor; //kolor tła z bootstrapa
         this.element = document.createElement("div");
@@ -20,13 +19,17 @@ class taskGroup {
         taskView.render();
     }
     render() {
+        const progress = Math.round((4 / this.taskList.length) * 100); //zamiast tej '4' będize trzeba dodać zmienną z ilościa wykonanych tasków
         // Oczywiscie caly ten HTML można zmienić ja to zakodowalem tylko by pokazać koncepcje
         if (this.taskGroupStyle == 'list') {
             this.element.innerHTML = `<i class="fas fa-clipboard-list"></i>
                                         <h2>${this.taskGroupName}</h2>
                                         <div class="line"></div>
-                                        <div class = "progressBarBorder"></div>
-                                        <h4>0/${this.taskLength}</h4>`
+                                        <div class = "progressBar">
+                                            <div class = "progressBarBorder"></div>
+                                            <div class = "progressBarProgress barBG${this.bgcolor}" style="width: ${progress}%"></div>
+                                        </div>
+                                        <h4>4/${this.taskList.length}</h4>`
             document.getElementById('root').appendChild(this.element);
         }
         else {
