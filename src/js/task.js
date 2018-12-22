214b8e9f88ca2f89596665c02a7c8f2dee06c850
Marcin <marcinwos18@gmail.com>
2018-12-22 20:48:22 +0100

    "Merge ikonek Ani"

--- src/js/task.js
+++ src/js/task.js
@@ -1,5 +1,8 @@
 import Query from './query';
-
+import check from '../img/003-check.png';
+import rubbish from '../img/001-rubbish.png';
+import done from '../img/002-doodles.png';
+import bear from '../img/bear.png';
 class Task {
 
     constructor(name, bgcolor) {
@@ -10,14 +13,28 @@ class Task {
         this.element.className = `btn btn-primary btn-lg btn-block ${this.bgcolor}`
         this.element.onclick = this.click;
     }
-    click = () => {
+
+
+
+    click = (e) => {
         //co ma sie zadziać po kliknieciu
-        console.log(this.name);
-        // Query.test();    
+        console.log(e.target.className);
+     if (e.target.className==="icons done" ){
+e.target.parentNode.nextSibling.nextSibling.style.textDecoration = "line-through";
+
+e.target.src=`${bear}`;
+
+
     }
+    }
+        // Query.test();
+    
     render() {
-        this.element.innerHTML = `<p>${this.name}</p>`;
-        document.getElementById('root').appendChild(this.element);
+       this.element.innerHTML = `<button class="button"><img class="icons done" src="${check}"/></button>
+        <p>${this.name}</p> 
+        <button class="button"> <img  class="icons"  src="${rubbish}"/></button> <button class="button">
+        <img class="icons" src="${done}"/></button>`;
+       document.getElementById('root').appendChild(this.element);
     }
 }
 
