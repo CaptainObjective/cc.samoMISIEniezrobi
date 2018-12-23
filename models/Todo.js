f20720c4190b7e973fe02cbfdbc46689543777c4
Wojciech Opulski <wojtekopulski@gmail.com>
2018-12-23 16:08:25 +0100

    Todo schema update

--- models/Todo.js
+++ models/Todo.js
@@ -1,9 +1,24 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema({
-    title:{
-type: String,
-required: true
-    } 
+    groupType: {
+        type: String,
+        enum: ["job", "shop", "toVisit"],
+        required: true,
+    },
+    importance: {
+        type: Number,
+        enum: [1, 2, 3], // 1 - main priority / 2 - less important // 3 - don't really have to be done quickly
+        default: 1,
+    },
+    isDone: {
+        type: Boolean,
+        default: false,
+        required: false,
+    },
+    title: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = mongoose.model ('todo', todoSchema);
+module.exports = mongoose.model('todo', todoSchema);
